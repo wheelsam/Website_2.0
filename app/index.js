8736c632d7dd65361d1ac836203875ae9c454364
wheels4 <wheels4@uw.edu>
2020-09-18 17:46:41 -0700

    Adding in swiping

--- app/index.js
+++ app/index.js
@@ -1,44 +1,92 @@
 function showabout(){
+    detectswipe('about_container', closeabout);
     $("#about_container").css("display","inherit");
     $("#about_container").addClass("animated slideInLeft");
     setTimeout(function(){
         $("#about_container").removeClass("animated slideInLeft");
     },800);
 }
-function closeabout(){
-    $("#about_container").addClass("animated slideOutLeft");
-    setTimeout(function(){
-        $("#about_container").removeClass("animated slideOutLeft");
-        $("#about_container").css("display","none");
-    },800);
+function closeabout(direct){
+    if (!direct || direct === 'l') {
+      $("#about_container").addClass("animated slideOutLeft");
+      setTimeout(function(){
+          $("#about_container").removeClass("animated slideOutLeft");
+          $("#about_container").css("display","none");
+      },800);
+    }
 }
 function showwork(){
+    detectswipe('work_container', closework);
     $("#work_container").css("display","inherit");
     $("#work_container").addClass("animated slideInRight");
     setTimeout(function(){
         $("#work_container").removeClass("animated slideInRight");
     },800);
 }
-function closework(){
-    $("#work_container").addClass("animated slideOutRight");
-    setTimeout(function(){
-        $("#work_container").removeClass("animated slideOutRight");
-        $("#work_container").css("display","none");
-    },800);
+function closework(direct){
+    if (!direct || direct === 'r') {
+      $("#work_container").addClass("animated slideOutRight");
+      setTimeout(function(){
+          $("#work_container").removeClass("animated slideOutRight");
+          $("#work_container").css("display","none");
+      },800);
+    }
 }
 function showcontact(){
+    detectswipe('contact_container', closecontact);
     $("#contact_container").css("display","inherit");
     $("#contact_container").addClass("animated slideInUp");
     setTimeout(function(){
         $("#contact_container").removeClass("animated slideInUp");
     },800);
 }
-function closecontact(){
-    $("#contact_container").addClass("animated slideOutDown");
-    setTimeout(function(){
-        $("#contact_container").removeClass("animated slideOutDown");
-        $("#contact_container").css("display","none");
-    },800);
+function closecontact(direct){
+    if (!direct || direct === 'd') {
+      $("#contact_container").addClass("animated slideOutDown");
+      setTimeout(function(){
+          $("#contact_container").removeClass("animated slideOutDown");
+          $("#contact_container").css("display","none");
+      },800);
+    }
+}
+function detectswipe(el,func){
+    swipe_det = new Object();
+    swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+    var min_x = 30;  //min x swipe for horizontal swipe
+    var max_x = 30;  //max x difference for vertical swipe
+    var min_y = 50;  //min y swipe for vertical swipe
+    var max_y = 60;  //max y difference for horizontal swipe
+    var direc = "";
+    ele = document.getElementById(el);
+    ele.addEventListener('touchstart',function(e){
+        var t = e.touches[0];
+        swipe_det.sX = t.screenX;
+        swipe_det.sY = t.screenY;
+    },false);
+    ele.addEventListener('touchmove',function(e){
+        e.preventDefault();
+        var t = e.touches[0];
+        swipe_det.eX = t.screenX;
+        swipe_det.eY = t.screenY;
+    },false);
+    ele.addEventListener('touchend',function(e){
+        //horizontal detection
+        if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y) && (swipe_det.eX > 0)))) {
+            if(swipe_det.eX > swipe_det.sX) direc = "r";
+            else direc = "l";
+        }
+        //vertical detection
+        else if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x) && (swipe_det.eY > 0)))) {
+            if(swipe_det.eY > swipe_det.sY) direc = "d";
+            else direc = "u";
+        }
+
+        if (direc != "") {
+            if(typeof func == 'function') func(direc);
+        }
+        direc = "";
+        swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+    },false);
 }
 setTimeout(function(){
     $("#loading").addClass("animated fadeOut");
